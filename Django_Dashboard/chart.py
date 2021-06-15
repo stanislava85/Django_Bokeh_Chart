287ebd8bf22a4be1e47839c228c29bffaa9d9afb
stanislava85 <stani85@gmail.com>
2021-06-14 20:22:46 -0400

    adding chart.py

--- Django_Dashboard/chart.py
+++ Django_Dashboard/chart.py
@@ -1,1 +1,22 @@
-        1 file(s) copied.
+ import pandas as pd
+import bokeh
+from bokeh.plotting import figure
+from bokeh.io import show
+
+def chart_func():
+    df = pd.read_csv('../datasets/appstore_games_march_2021.csv', low_memory=False)
+    df = df.dropna()
+    df.loc[df['price'].str.contains('Free'), 'price'] = '0'
+    df['price']= df['price'].str.replace('$', '')
+    df['price'] = pd.to_numeric(df['price'],errors='coerce')
+    df['release_date'] = pd.to_datetime(df['release_date'], format = '%Y/%m/%d')
+    df_date = pd.DataFrame({'price':df['price']})
+    df_date = df_date.set_index(df['release_date'])
+    df_date = df_date.sort_values(by=['release_date'])
+    fig = figure(x_axis_type='datetime',
+             plot_height=650, plot_width=1350,
+             title='Date vs App Price')
+	fig.line(y='price', x='release_date', source=df_date)
+	return fig
+
+chart_func()
--- Django_Dashboard/views.py
+++ Django_Dashboard/views.py
@@ -10,16 +10,6 @@ from django.shortcuts import render
 
 
 def index(request):
-    n = 50000
-    x = np.random.standard_normal(n)
-    y = np.random.standard_normal(n)
-    bins = hexbin(x, y, 0.3)
-    p = figure(title="", tools="wheel_zoom,pan,reset",
-               match_aspect=True, background_fill_color="#fff", 
-               sizing_mode="scale_both")
-    p.grid.visible= False
-    p.hex_tile(q="q", r="r", size=0.1, line_color=None, source=bins,
-               fill_color=linear_cmap('counts', 'Viridis256', 0, max(bins.counts)))
-    script, div = components(p)
+    
 
     return render(request, 'index.html', {'div': div, 'script':script})
